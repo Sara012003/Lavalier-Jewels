490b0662c100563bc46dbba681ba63fe38ae7efb
Sara012003 <sarag4063@gmail.com>
2024-11-26 18:47:50 -0300

    Change in Taiwlind CSS configuration path structure

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',         // Archivo HTML principal
+    './src/**/*.{html,css}'  // Archivos dentro de src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
